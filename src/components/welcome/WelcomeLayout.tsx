b637295e670fd3ab1384fddc1eeda0da455c276f
liuk <974487599@qq.com>
2023-08-03 12:04:46 +0800

    重构welcome相关组件

--- src/components/welcome/WelcomeLayout.tsx
+++ src/components/welcome/WelcomeLayout.tsx
@@ -1,18 +1,18 @@
-import { defineComponent } from 'vue';
+import { FunctionalComponent } from 'vue';
 import s from './WelcomeLayout.module.scss';
-export const WelcomeLayout = defineComponent({
-    setup: (props, context) => {
-        const { slots } = context
-        return () => (
-            <div class={s.wrapper}>
-                <div class={s.card}>
-                    {slots.icon?.()}
-                    {slots.title?.()}
-                </div>
-                <div class={s.actions}>
-                    {slots.buttons?.()}
-                </div>
+export const WelcomeLayout: FunctionalComponent = (props, context) => {
+    const { slots } = context
+    return (
+        <div class={s.wrapper}>
+            <div class={s.card}>
+                {slots.icon?.()}
+                {slots.title?.()}
             </div>
-        )
-    }
-})
+            <div class={s.actions}>
+                {slots.buttons?.()}
+            </div>
+        </div>
+    )
+}
+
+WelcomeLayout.displayName = 'WelcomeLayout'
