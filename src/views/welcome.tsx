210c71f1776e12854288ae76a82bcbc75f75445a
liuk <974487599@qq.com>
2023-08-04 13:33:41 +0800

    增加节流

--- src/views/welcome.tsx
+++ src/views/welcome.tsx
@@ -2,15 +2,26 @@ import { defineComponent, Transition, VNode, ref, watchEffect } from "vue";
 import { RouterView, RouteLocationNormalizedLoaded, useRoute, useRouter } from "vue-router";
 import s from './Welcome.module.scss'
 import { useSwipe } from "../hooks/useSwipe";
+import { throttle } from "../utils/throttle";
+const pushMap: Record<string, string> = {
+    'welcome1': '/welcome/2',
+    'welcome2': '/welcome/3',
+    'welcome3': '/welcome/4',
+    'welcome4': '/start',
+}
 export const welcome = defineComponent({
     setup: (props, context) => {
         const main = ref<HTMLElement>()
         const { direction, swiping } = useSwipe(main)
         const route = useRoute()
         const router = useRouter()
+        const replace = throttle(() => {
+            const name = (route.name || 'welcome1').toString()
+            router.replace(pushMap[name])
+        }, 500)
         watchEffect(() => {
             if (swiping.value && direction.value === 'left') {
-                console.log('aaa')
+                replace()
             }
         })
         return () => <div class={s.wrapper}>
