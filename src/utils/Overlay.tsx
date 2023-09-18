8a9e1743ae7deadcb36c5357f7cd64b8b72c0012
liuk <974487599@qq.com>
2023-09-18 11:58:26 +0800

    封装backIcon 修改路由跳转

--- src/utils/Overlay.tsx
+++ src/utils/Overlay.tsx
@@ -1,61 +1,94 @@
-import { PropType, defineComponent, ref } from "vue";
-import { RouterLink } from 'vue-router';
+import { PropType, defineComponent, onMounted, ref } from "vue";
+import { RouterLink, useRoute } from "vue-router";
 import { Icon } from "./Icon";
-import s from './Overlay.module.scss';
+import s from "./Overlay.module.scss";
+import { useMeStore } from "../stores/useMeStore";
+import { showDialog } from "vant";
+import "vant/es/dialog/style";
 export const Overlay = defineComponent({
-    props: {
-        onClose: {
-            type: Function as PropType<() => void>
-        }
+  props: {
+    onClose: {
+      type: Function as PropType<() => void>,
     },
-    setup: (props, context) => {
-        const close = () => {
-            // props.onClose?.()
-            context.emit('close')
-        }
-        const onClickSignIn = () => { }
-        return () => (
-            <>
-                <div class={s.mask} onClick={close}></div>
-                <div class={s.overlay}>
-                    <section class={s.currentUser} onClick={onClickSignIn}>
-                        <h2>未登录用户</h2>
-                        <p>点击这里登陆</p>
-                    </section>
-                    <nav>
-                        <ul>
-                            <li>
-                                <RouterLink to="/statistics" class={s.action}>
-                                    <Icon name="charts" class={s.icon} /><span>统计图表</span>
-                                </RouterLink>
-                            </li>
-                            <li>
-                                <RouterLink to="/statistics" class={s.action}>
-                                    <Icon name="export" class={s.icon} /><span>导出数据</span>
-                                </RouterLink>
-                            </li>
-                            <li>
-                                <RouterLink to="/statistics" class={s.action}>
-                                    <Icon name="notify" class={s.icon} /><span>记账提醒</span>
-                                </RouterLink>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </>
-        )
-    }
-})
+  },
+  setup: (props, context) => {
+    const meStore = useMeStore();
+    const close = () => {
+      props.onClose?.();
+    };
+    const route = useRoute();
+    const me = ref<User>();
+    onMounted(async () => {
+      const response = await meStore.mePromise;
+      me.value = response?.data.resource;
+    });
+    const onSignOut = async () => {
+      await showDialog({
+        title: "确认",
+        message: "你真的要退出登录吗？",
+      });
+      localStorage.removeItem("jwt");
+      window.location.reload();
+    };
+    const onClickSignIn = () => {};
+    return () => (
+      <>
+        <div class={s.mask} onClick={close}></div>
+        <div class={s.overlay}>
+          <section class={s.currentUser}>
+            {me.value ? (
+              <div>
+                <h2 class={s.email}>{me.value.email}</h2>
+                <p onClick={onSignOut}>点击这里退出登录</p>
+              </div>
+            ) : (
+              <RouterLink to={`/sign_in?return_to=${route.fullPath}`}>
+                <h2>未登录用户</h2>
+                <p>点击这里登录</p>
+              </RouterLink>
+            )}
+          </section>
+          <nav>
+            <ul>
+              <li>
+                <RouterLink to="/statistics" class={s.action}>
+                  <Icon name="charts" class={s.icon} />
+                  <span>统计图表</span>
+                </RouterLink>
+              </li>
+              <li>
+                <RouterLink to="/export" class={s.action}>
+                  <Icon name="export" class={s.icon} />
+                  <span>导出数据</span>
+                </RouterLink>
+              </li>
+              <li>
+                <RouterLink to="/notify" class={s.action}>
+                  <Icon name="notify" class={s.icon} />
+                  <span>记账提醒</span>
+                </RouterLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </>
+    );
+  },
+});
 
 export const OverlayIcon = defineComponent({
-    setup: (props, context) => {
-        const refOverlayVisiable = ref(false)
-        const onclick = () => {
-            refOverlayVisiable.value = !refOverlayVisiable.value
-        }
-        return () => <>
-            <Icon name="menu" onClick={onclick} />
-            {refOverlayVisiable.value && <Overlay onClose={() => refOverlayVisiable.value = false} />}
-        </>
-    }
-})
+  setup: (props, context) => {
+    const refOverlayVisiable = ref(false);
+    const onclick = () => {
+      refOverlayVisiable.value = !refOverlayVisiable.value;
+    };
+    return () => (
+      <>
+        <Icon name="menu" onClick={onclick} />
+        {refOverlayVisiable.value && (
+          <Overlay onClose={() => (refOverlayVisiable.value = false)} />
+        )}
+      </>
+    );
+  },
+});
