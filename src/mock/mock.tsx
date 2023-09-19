ca6c6162d76b7b1e658de9fcc53e61c5b1f13905
liuk <974487599@qq.com>
2023-09-19 17:02:37 +0800

    处理echarts 渲染问题

--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -12,11 +12,11 @@ export const mockItemSummary: Mock = (config) => {
       200,
       {
         groups: [
-          { happen_at: "2022-07-18T00:00:00.000+0800", amount: 100 },
-          { happen_at: "2022-07-22T00:00:00.000+0800", amount: 300 },
+          { happen_at: "2022-07-18T00:00:00.000+0800", amount: 200 },
+          { happen_at: "2022-07-22T00:00:00.000+0800", amount: 400 },
           { happen_at: "2022-07-29T00:00:00.000+0800", amount: 200 },
         ],
-        summary: 600,
+        summary: 800,
       },
     ];
   } else if (group_by === "happen_at" && kind === "income") {
@@ -24,11 +24,11 @@ export const mockItemSummary: Mock = (config) => {
       200,
       {
         groups: [
-          { happen_at: "2022-07-08T00:00:00.000+0800", amount: 100 },
-          { happen_at: "2022-07-12T00:00:00.000+0800", amount: 300 },
+          { happen_at: "2022-07-08T00:00:00.000+0800", amount: 300 },
+          { happen_at: "2022-07-12T00:00:00.000+0800", amount: 600 },
           { happen_at: "2022-07-19T00:00:00.000+0800", amount: 200 },
         ],
-        summary: 600,
+        summary: 1100,
       },
     ];
   } else if (group_by === "tag_id" && kind === "expenses") {
--- src/views/StatisticsPage.tsx
+++ src/views/StatisticsPage.tsx
@@ -1,11 +1,8 @@
 import { defineComponent, ref } from "vue";
-import s from './StatisticsPage.module.scss';
 import { TimeLayout } from "../layouts/TimeLayout";
 import { Charts } from "../components/statistics/Charts";
 export const StatisticsPage = defineComponent({
-    setup: (props, context) => {
-        return () => <>
-            <TimeLayout component={Charts} />
-        </>
-    }
-})
+  setup: (props, context) => {
+    return () => <TimeLayout rerenderOnSwitchTab={true} component={Charts} />;
+  },
+});
